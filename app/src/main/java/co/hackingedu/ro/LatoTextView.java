b8350dcffc008aa78d2be7a3217b9b41f87767ae
rrtigga <rrtigga@ucdavis.edu>
2015-09-15 18:07:41 -0700

    Lato font all throughout

---
+++ app/src/main/java/co/hackingedu/ro/LatoTextView.java
@@ -0,0 +1,17 @@
+package co.hackingedu.ro;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Spicycurryman on 9/15/15.
+ */
+public class LatoTextView extends TextView {
+
+    public LatoTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/Lato-Regular.ttf"));
+    }
+}
